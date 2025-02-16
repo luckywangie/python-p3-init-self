b103ac6f8625798f1fd99590c2d409593e21bb7e
lucky wangie <wangiemamati@gmail.com>
2025-02-16 11:14:55 +0300

    tests passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
-
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+#test
+if __name__ == "__main__":
+    fido = Dog("Fido", "Golden Retriever")
+    print(f"{fido.name} is a {fido.breed}")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
-
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+
+#test
+if __name__ == "__main__":
+    person1 = Person("John Doe")
+    print(f"The person's name is {person1.name}")
